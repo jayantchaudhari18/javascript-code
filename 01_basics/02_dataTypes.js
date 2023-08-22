1817311b58a1fef4192c4f230a082b9ed7e409fe
Jayant Chaudhari <110541681+jayantchaudhari18@users.noreply.github.com>
2023-08-22 19:19:21 +0000

    Learned about datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+let age=10;
+let name="jayant";
+
+//number =>2 to the power 23
+//string=>""
+//bigInt
+//boolean=>true,false
+//null
+//undefined
+
+console.log(typeof null);//type is object
+console.log(typeof undefined);//type is undefined

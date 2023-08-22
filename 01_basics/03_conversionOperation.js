c2916d414567fb796c781af204dc74d3304276cb
Jayant Chaudhari <110541681+jayantchaudhari18@users.noreply.github.com>
2023-08-22 20:16:43 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let num="33";
+
+//null gets converted to zero....Number(null)
+//String if converted into number then it gets converted into NaN
+//"33"=>33
+//"33dsd"=>NaN
+//TRUE=>1 FALSE=>0
+//NULL=>0
+
+console.log(typeof(num));
+
+let valueInNumber=Number(num);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//to BOOLEAN 
+//1=>true
+//0=>false
+//""=>false
+//"jayant"=>1
+
+//to string
+let num2=33;
+let isnum=String(num2);
+console.log(isnum);
+
+
+
